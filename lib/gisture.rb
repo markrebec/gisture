1c8517a3e8b3e73d9720e269dd4a96558fe33778
Mark Rebec <mark@markrebec.com>
2015-06-02 18:43:07 -0700

    adds a canfig for gisture and adds basic auth config options for the github_api gem

--- lib/gisture.rb
+++ lib/gisture.rb
@@ -1,4 +1,15 @@
 require 'gisture/version'
 
 module Gisture
+  include Canfig::Module
+
+  configure do |config|
+    # config options for the github_api gem
+    config.basic_auth     = nil # user:password string
+    config.oauth_token    = nil # oauth authorization token
+    config.client_id      = nil # oauth client id
+    config.client_secret  = nil # oauth client secret
+    config.user           = nil # global user used in requets if none provided
+    config.org            = nil # global organization used in request if none provided
+  end
 end
