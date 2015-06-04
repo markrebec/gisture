3e2475a535d53e1e2289ac9d5b5fd0b772cc928a
Mark Rebec <mark@markrebec.com>
2015-06-03 20:19:40 -0700

    monkeypatches github_api with a method to fetch a specific version of a gist

---
+++ lib/github_api/client/gists.rb
@@ -0,0 +1,9 @@
+module Github
+  class Client::Gists < API
+    def version(*args)
+      arguments(args, required: [:id, :version])
+
+      get_request("/gists/#{arguments.id}/#{arguments.version}", arguments.params)
+    end
+  end
+end
