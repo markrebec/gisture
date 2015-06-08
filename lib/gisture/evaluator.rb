bd1589ebd5b2e7125dd8304bcdbd3bca76f1633d
Mark Rebec <mark@markrebec.com>
2015-06-08 16:50:40 -0700

    refactors evaluator a little bit and adds specs

--- lib/gisture/evaluator.rb
+++ lib/gisture/evaluator.rb
@@ -1,12 +1,16 @@
 module Gisture
   class Evaluator
-    attr_reader :raw
+    attr_reader :raw, :result
+
+    def evaluate
+      instance_eval { @result = eval raw }
+    end
 
     protected
     
     def initialize(raw)
-      @raw = raw
-      instance_eval { eval raw }
+      @raw = raw.to_s
+      evaluate
     end
   end
 end
--- spec/gisture/evaluator_spec.rb
+++ spec/gisture/evaluator_spec.rb
@@ -1,4 +1,27 @@
-require 'spec_helper'
+require "spec_helper"
 
 RSpec.describe Gisture::Evaluator do
+  it "requires a string to be eval'd" do
+    expect { Gisture::Evaluator.new }.to raise_exception(ArgumentError)
+  end
+
+  it "converts raw input to a string" do
+    expect { Gisture::Evaluator.new(nil) }.to_not raise_exception
+  end
+
+  describe "#raw" do
+    subject { Gisture::Evaluator.new("1+1") }
+
+    it "stores the raw string passed on init" do
+      expect(subject.raw).to eql("1+1")
+    end
+  end
+
+  describe "#result" do
+    subject { Gisture::Evaluator.new("1+1") }
+
+    it "stores the result returned by evaluating the block" do
+      expect(subject.result).to eql(2)
+    end
+  end
 end
