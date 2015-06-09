dc8e88652a3f367ca708fd6a6038031a43d50414
Mark Rebec <mark@markrebec.com>
2015-06-08 18:22:42 -0700

    add optional oauth token config for running specs to avoid unauthed rate limiting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,10 @@ Coveralls.wear!
 
 #Dir[File.join(File.dirname(__FILE__), '..', "spec/support/**/*.rb")].each { |f| require f }
 
+Gisture.configure do |config|
+  config.oauth_token = ENV['GITHUB_OAUTH_TOKEN']
+end
+
 RSpec.configure do |config|
   #config.before(:suite) do
   #  ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
