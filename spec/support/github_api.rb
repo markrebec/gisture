5def3f734d4c57478db0f0c0586c5302070a4bb2
Mark Rebec <mark@markrebec.com>
2015-07-08 21:16:19 -0700

    patches relevant github_api methods to use vcr when running specs

--- spec/support/github_api.rb
+++ spec/support/github_api.rb
@@ -1,16 +1,57 @@
+require 'github_api/client/gists'
+require 'github_api/client/repos'
+require 'github_api/client/repos/contents'
+
 module Github
   class Client::Gists < API
-    def get(*args)
+    def get_with_vcr(*args)
       arguments(args, required: [:id])
 
-      TEST_GISTS.to_a.find { |g| g.first == arguments.id }.last
+      VCR.use_cassette("gists/#{arguments.id}") do
+        get_without_vcr(*args)
+      end
     end
+    alias_method :get_without_vcr, :get
+    alias_method :get, :get_with_vcr
+    alias_method :find_without_vcr, :find
+    alias_method :find, :get_with_vcr
 
-    def version(*args)
+    def version_with_vcr(*args)
       arguments(args, required: [:id, :version])
 
-      # TODO implement this
-      nil
+      VCR.use_cassette("gists/#{arguments.id}-#{arguments.version}") do
+        super
+      end
     end
+    alias_method :version_without_vcr, :version
+    alias_method :version, :version_with_vcr
+  end
+
+  class Client::Repos < API
+    def get_with_vcr(*args)
+      arguments(args, required: [:user, :repo])
+
+      VCR.use_cassette("repos/#{arguments.user}-#{arguments.repo}") do
+        get_without_vcr(*args)
+      end
+    end
+    alias_method :get_without_vcr, :get
+    alias_method :get, :get_with_vcr
+    alias_method :find_without_vcr, :find
+    alias_method :find, :get_with_vcr
+  end
+
+  class Client::Repos::Contents < API
+    def get_with_vcr(*args)
+      arguments(args, required: [:user, :repo, :path])
+
+      VCR.use_cassette("repos/#{arguments.user}/#{arguments.repo}/#{arguments.path}") do
+        get_without_vcr(*args)
+      end
+    end
+    alias_method :get_without_vcr, :get
+    alias_method :get, :get_with_vcr
+    alias_method :find_without_vcr, :find
+    alias_method :find, :get_with_vcr
   end
 end
