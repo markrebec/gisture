010dedce10456272889a70b8ca13e096163f9e2c
Mark Rebec <mark@markrebec.com>
2015-07-17 10:51:08 -0700

    fix path references in hello world examples

--- examples/hello_world/resourceful.rb
+++ examples/hello_world/resourceful.rb
@@ -1,3 +1,3 @@
 # example of reading a resource that was included in a .gist
-puts ::File.read('examples/resourceful.txt')
+puts ::File.read('examples/hello_world/resourceful.txt')
 puts ::File.read('lib/gisture/version.rb')
